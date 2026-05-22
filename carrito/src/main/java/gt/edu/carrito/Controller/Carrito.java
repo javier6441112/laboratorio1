e47617658b877594e900b6033f2b3c5e8c579b2c
Javier <jisaacsand@gmail.com>
2026-05-21 20:47:23 -0600

    se agraga configuracion para subir a nube

--- carrito/src/main/java/gt/edu/carrito/Controller/Carrito.java
+++ carrito/src/main/java/gt/edu/carrito/Controller/Carrito.java
@@ -3,28 +3,33 @@ package gt.edu.carrito.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gt.edu.carrito.Dto.ProductoDto;
 import gt.edu.carrito.Services.CarritoService;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/carrito")
 public class Carrito {
 
     @Autowired
     private CarritoService carritoService;
 
-    @GetMapping("/getCarrito")
+    @GetMapping("/")
+    public String inicio() {
+        return "Bienvenido al carrito de compras. Accede a /carrito/getCarrito o /carrito/agregarProducto";
+    }
+
+    @GetMapping("/carrito/getCarrito")
     public List<ProductoDto> getCarrito() {
         return carritoService.getCarrito();
     }
 
-    @PostMapping("/agregarProducto")
+    @PostMapping("/carrito/agregarProducto")
     public ProductoDto agregarProducto(@RequestBody ProductoDto productoDto) {
         return carritoService.guardarProducto(productoDto);
     }
